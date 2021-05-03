35b123a2f32ac84ff1491f0df16c746d2384979c
tinyhhj <krwsim@gmail.com>
2021-05-04 02:07:42 +0900

    feat: 우승자 결정하는 로직 추가
    RacingPlayer

--- src/main/java/racingcar/domain/RacingPlayer.java
+++ src/main/java/racingcar/domain/RacingPlayer.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class RacingPlayer {
@@ -17,12 +18,25 @@ public class RacingPlayer {
 	}
 
 	public List<Car> calculateWinner() {
-		List<Car> winners = new ArrayList<>(players);
-		Collections.sort(winners);
-		int idx = winners.size()-1;
-		while (idx >= 0) {
+		int winnerDistance = getWinnerDistance();
+		List<Car> winners = new ArrayList<>();
+		for (Car player : players) {
+			addPlayerIfWinner(winners, player,winnerDistance);
 		}
-		System.out.println(players);
-		return null;
+		return winners;
+	}
+
+	private void addPlayerIfWinner(List<Car> winners, Car player, int winnerDistance) {
+		if (winnerDistance == player.getDistance()) {
+			winners.add(player);
+		}
+	}
+
+	private int getWinnerDistance() {
+		int winnerDistance = 0;
+		for (Car player: players) {
+			winnerDistance = Math.max(winnerDistance, player.getDistance());
+		}
+		return winnerDistance;
 	}
 }
--- src/test/java/racingcar/domain/RacingPlayerTest.java
+++ src/test/java/racingcar/domain/RacingPlayerTest.java
@@ -1,22 +1,47 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RacingPlayerTest {
+	List<Car> players;
+
+	@BeforeEach
+	public void setup() {
+		players = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			CarInfo info = new CarInfo("test" + i);
+			players.add(new TestCar(info, 4 - i));
+		}
+	}
 
 	@Test
+	@DisplayName("우승자가 한명일 경우 테스트")
 	public void winnerTest() {
-		List<Car> players = new ArrayList<>();
-		for (int i = 0; i < 5; i++) {
-			CarInfo info = new CarInfo("test"+i);
-			players.add(new TestCar(info,4-i));
-		}
 		RacingPlayer racingPlayer = new RacingPlayer(players);
 		List<Car> winners = racingPlayer.calculateWinner();
 
+		assertThat(winners)
+			.hasSize(1)
+			.contains(players.get(0));
+	}
+
+	@Test
+	@DisplayName("우승자가 중복일경우 테스트")
+	public void winnerTest2() {
+		players.add(new TestCar(new CarInfo("test5"), 4));
+		RacingPlayer racingPlayer = new RacingPlayer(players);
+		List<Car> winners = racingPlayer.calculateWinner();
+
+		assertThat(winners)
+			.hasSize(2)
+			.containsExactly(players.get(0), players.get(5));
 	}
 }
